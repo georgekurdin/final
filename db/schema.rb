245086185161081d8362c7cb9222a2dfc54ffad8
vagrant <george.kourdin@gmail.com>
2014-06-10 20:39:54 +0000

    First commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140610191512) do
+=======
+ActiveRecord::Schema.define(version: 20140610195542) do
+>>>>>>> hopefully final version
 
   create_table "comments", force: true do |t|
     t.integer  "rating"
