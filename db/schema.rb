9638de71ac75729679bceea38e5041dfaf1dea5f
vagrant <vagrant@kellogg-dev-box>
2014-06-10 18:59:21 +0000

    hopefully final version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610182621) do
+ActiveRecord::Schema.define(version: 20140610184407) do
 
   create_table "comments", force: true do |t|
     t.integer  "rating"
