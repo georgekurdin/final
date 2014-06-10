89ea828134e529066cf276ab957b89acd859f340
vagrant <george.kourdin@gmail.com>
2014-06-10 19:17:53 +0000

    leeerooy jeeeeenkins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610184407) do
+ActiveRecord::Schema.define(version: 20140610191512) do
 
   create_table "comments", force: true do |t|
     t.integer  "rating"
